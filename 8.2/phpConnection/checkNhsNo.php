8f8a667963d004ee4f4dbae5a8b2cc5e9f3a5228
w1820984-NipunLuca <w1820984@my.westminster.ac.uk>
2023-04-20 00:03:20 +0100

    registration_nhsnumber

--- 8.2/phpConnection/checkNhsNo.php
+++ 8.2/phpConnection/checkNhsNo.php
@@ -1,26 +1,33 @@
-
 <?php
 header("Access-Control-Allow-Origin: *");
+
 // Connect to central database
 $central_db = new PDO('sqlite:vaccines.db');
 
 // Get the nhs number from the AJAX post request
 $nhs_number = $_POST['nhsNumber'];
 
-// Get patient record from central database
-$check_nhs_number = $central_db->prepare('SELECT * FROM patients WHERE NHSNumber = :nhs_number');
+// Check if NHS number exists in central database
+$check_nhs_number = $central_db->prepare('SELECT NHSNumber FROM patients WHERE NHSNumber = :nhs_number');
 $check_nhs_number->bindParam(':nhs_number', $nhs_number);
 $check_nhs_number->execute();
-$patient_record = $check_nhs_number->fetch(PDO::FETCH_ASSOC);
+$nhs_number_result = $check_nhs_number->fetchColumn();
 
-// Check if patient record exists in central database
-if ($patient_record) {
-    // Return patient record as a JSON object to the registration page
-    header('Content-Type: application/json');
-    echo json_encode($patient_record);
+// Check if NHS number exists in GpSurgery database
+$gp_db = new PDO('sqlite:GpSurgery.db');
+$check_gp_number = $gp_db->prepare('SELECT NHSNumber FROM LocalPatient WHERE NHSNumber = :nhs_number');
+$check_gp_number->bindParam(':nhs_number', $nhs_number);
+$check_gp_number->execute();
+$gp_number_result = $check_gp_number->fetchColumn();
+
+if ($gp_number_result) {
+    // NHS number is already registered to our local database
+    echo "This NHS number is already registered to our local database";
+} else if ($nhs_number_result) {
+    // Return NHS number as a string to the registration page
+    echo $nhs_number_result;
 } else {
     // NHSNumber does not exist in central database
     echo "NHSNumber does not exist in central database";
-} 
-
+}
 ?>
