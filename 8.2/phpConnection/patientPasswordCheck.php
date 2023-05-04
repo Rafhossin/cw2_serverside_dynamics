0aa47c64857a83ae9a69da2cdd50ad36c47873d8
Khandakar Hossin <krhossin@yahoo.co.uk>
2023-05-04 12:01:32 +0100

    new change

--- 8.2/phpConnection/patientPasswordCheck.php
+++ 8.2/phpConnection/patientPasswordCheck.php
@@ -25,39 +25,4 @@ if ($patient_record) {
     echo json_encode(array('success' => false));
 }
 
-?> -->
-
-<?php
-header("Access-Control-Allow-Origin: *");
-$local_db = new PDO('sqlite:GpSurgery.db');
-$password = $_POST['password'];
-$email = $_POST['emailAddress'];
-$role = $_POST['role'];
-
-if ($role == "doctor") {
-    $sql = "SELECT DoctorId, DoctorPassword FROM Doctor WHERE DoctorEmail = :email";
-} else if ($role == "receptionist") {
-    $sql = "SELECT ReceptionistId, ReceptionistPassword FROM Receptionist WHERE ReceptionistEmail = :email";
-}
-
-$stmt = $local_db->prepare($sql);
-$stmt->bindParam(':email', $email);
-$stmt->execute();
-
-if ($stmt->rowCount() == 1) {
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    $stored_password = ($role == "doctor") ? $result['DoctorPassword'] : $result['ReceptionistPassword'];
-    $id_result = ($role == "doctor") ? $result['DoctorId'] : $result['ReceptionistId'];
-
-    if (password_verify($password, $stored_password)) {
-        // Return true with the doctor or receptionist ID
-        echo json_encode(array('success' => true, 'id' => $id_result));
-    } else {
-        // Return false
-        echo json_encode(array('success' => false));
-    }
-} else {
-    // Return false
-    echo json_encode(array('success' => false));
-}
 ?>
